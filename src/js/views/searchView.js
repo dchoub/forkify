c1802b28e6c1f1bd58ddc301edcdd82f27d9dbce
Dhwani Choubisa <dhwchoub@publicisgroupe.net>
2019-02-09 23:28:46 +0000

    adding the recipe model and added control recipe in index

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -6,23 +6,23 @@ import {element} from './base';
 
 export const getInput = () => element.searchInput.value;
 export const clearInput = () => {
-    element.searchInput.value ='';
+    element.searchInput.value = '';
 };
-export const clearResult = ()=>{
-    element.searchResultList.innerHTML ='';
-    element.searchResultPage.innerHTML='';
+export const clearResult = ()=> {
+    element.searchResultList.innerHTML = '';
+    element.searchResultPage.innerHTML = '';
 };
 
-const limitrecipeTitle = (title, limit=17) => {
+const limitrecipeTitle = (title, limit = 17) => {
     //to add in array
-    const newTitle =[];
-    if(title.length > limit){
-        title.split(' ').reduce((acc, cur) =>{
-            if(acc + cur.length <=limit){
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
                 newTitle.push(cur);
             }
-            return acc +cur.length;
-        },0);
+            return acc + cur.length;
+        }, 0);
         return `${newTitle.join(' ')} ...`
 
     }
@@ -40,39 +40,34 @@ const createButton = (page, type)=>`
 `;
 
 
-
-const renderButton = (page, numResults,  resperPage) =>{
+const renderButton = (page, numResults, resperPage) => {
     const pages = Math.ceil(numResults / resperPage);
     let button;
-    if(page===1 && pages >1){
+    if (page === 1 && pages > 1) {
         //show next button
-       button = createButton(page,'next');
+        button = createButton(page, 'next');
     }
-    else if(page < pages){
+    else if (page < pages) {
         //show both the button
-       button = `
+        button = `
             ${createButton(page, 'prev')}
             ${createButton(page, 'next')}
         `;
     }
-    else if (page === pages && pages >1)
-    {
+    else if (page === pages && pages > 1) {
         //show only prev button
-        button = createButton(page,'prev')
+        button = createButton(page, 'prev')
     }
-element.searchResultPage.insertAdjacentHTML('afterbegin', button);
+    element.searchResultPage.insertAdjacentHTML('afterbegin', button);
 
 
 };
 
 
-
-
-
-const renderRecipe = recipe =>{
-    const markup =`
+const renderRecipe = recipe => {
+    const markup = `
      <li>
-                <a class="results__link results" href=${recipe.recipe_id}>
+                <a class="results__link results" href=#${recipe.recipe_id}>
                     <figure class="results__fig">
                         <img src=${recipe.image_url}" alt=${recipe.title}>
                     </figure>
@@ -86,12 +81,12 @@ const renderRecipe = recipe =>{
     element.searchResultList.insertAdjacentHTML('beforeend', markup)
 };
 
-export const renderResult = (recipe, page=1, resPerPage = 10)=>{
+export const renderResult = (recipe, page = 1, resPerPage = 10)=> {
     //getting results per page
-    const start = (page -1) * resPerPage;
+    const start = (page - 1) * resPerPage;
     const end = page * resPerPage;
     recipe.slice(start, end).forEach(renderRecipe);
 
     //render pagination button
-    renderButton(page, recipe.length, resPerPage )
+    renderButton(page, recipe.length, resPerPage)
 };
